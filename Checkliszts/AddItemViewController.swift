9b8ac2794dcbc7b9568ddf9e4fde036df7fa62c9
turtelneck <65428036+turtelneck@users.noreply.github.com>
2024-05-07 18:33:11 -0500

    Update AddItemViewController.swift
    Changed to be more like book version

--- Checkliszts/AddItemViewController.swift
+++ Checkliszts/AddItemViewController.swift
@@ -22,27 +22,41 @@ protocol ItemDetailViewControllerDelegate: AnyObject {
 
 class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
     
-    weak var delegate: ItemDetailViewControllerDelegate?
-    var itemToEdit: ChecklistItem?
-    
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
+    weak var delegate: ItemDetailViewControllerDelegate?
+    var itemToEdit: ChecklistItem?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
         
         if let item = itemToEdit {
             title = "Edit Item"
             textField.text = item.text
             doneBarButton.isEnabled = true
         }
-        
-        navigationItem.largeTitleDisplayMode = .never
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         textField.becomeFirstResponder()
+    }
+    
+    // MARK: - Actions
+    @IBAction func cancel() {
+        delegate?.itemDetailViewControllerDidCancel(self)
+    }
+
+    @IBAction func done() {
+        if let item = itemToEdit {
+            item.text = textField.text!
+            delegate?.itemDetailViewController(self, didFinishEditing: item)
+        } else {
+            let item = ChecklistItem(textField.text!, false)
+            delegate?.itemDetailViewController(self, didFinishAdding: item)
+        }
     }
     
     // MARK: - Table View Delegates
@@ -61,28 +75,19 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
     ) -> Bool {
         let oldText = textField.text!
         let stringRange = Range(range, in: oldText)!
-        let newText = oldText.replacingCharacters(in: stringRange, with: string)
-        doneBarButton.isEnabled = !newText.isEmpty
+        let newText = oldText.replacingCharacters(
+            in: stringRange,
+            with: string)
+        if newText.isEmpty {
+          doneBarButton.isEnabled = false
+        } else {
+          doneBarButton.isEnabled = true
+        }
         return true
     }
     
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
         doneBarButton.isEnabled = false
         return true
-    }
-    
-    // MARK: - Actions
-    @IBAction func cancel() {
-        delegate?.itemDetailViewControllerDidCancel(self)
-    }
-
-    @IBAction func done() {
-        if let item = itemToEdit {
-            item.text = textField.text!
-            delegate?.itemDetailViewController(self, didFinishEditing: item)
-        } else {
-            let item = ChecklistItem(textField.text!, false)
-            delegate?.itemDetailViewController(self, didFinishAdding: item)
-        }
     }
 }
